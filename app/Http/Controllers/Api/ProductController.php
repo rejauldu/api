132162e08691d9ae39acf3772a244c2cab270001
Rejaul <rejaul@gmail.com>
2020-01-01 01:20:34 +0600

    Controllers, Resources and Routes created

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,97 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\ProductResource;
+use App\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+class ProductController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ProductResource::collection(Product::paginate(20));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request, Product $product)
+    {
+        $request['detail'] = $request->description;
+        unset($request['description']);
+		$product->create($product);
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $request['detail'] = $request->description;
+        unset($request['description']);
+        $product->update($request->all());
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/products','Api\ProductController');
+Route::apiResource('/products/{product}/reviews','Api\ReviewController');
 
 Route::group([ 'prefix' => 'auth'], function (){ 
     Route::group(['middleware' => ['guest:api']], function () {
